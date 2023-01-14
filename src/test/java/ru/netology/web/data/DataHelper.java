e91db8d1e62910390f0a89dcc72afff53eff36b9
Anastasiia Utina <anastasiiaprekrasnaiia@gmail.com>
2023-01-14 23:46:00 +0300

    update files

--- src/test/java/ru/netology/web/data/DataHelper.java
+++ src/test/java/ru/netology/web/data/DataHelper.java
@@ -3,67 +3,69 @@ package ru.netology.web.data;
 import lombok.Value;
 
 public class DataHelper {
-  private DataHelper() {}
+    private DataHelper() {
+    }
 
-  @Value
-  public static class AuthInfo {
-    private String login;
-    private String password;
-  }
+    @Value
+    public static class AuthInfo {
+        private String login;
+        private String password;
+    }
 
-  public static AuthInfo getAuthInfo() {
-    return new AuthInfo("vasya", "qwerty123");
-  }
+    public static AuthInfo getUserAuthInfo() {
+        return new AuthInfo("vasya", "qwerty123");
+    }
 
-  @Value
-  public static class VerificationCode {
-    private String code;
-  }
+    @Value
+    public static class VerificationCode {
+        private String code;
+    }
 
-  public static VerificationCode getVerificationCodeFor() {
-    return new VerificationCode("12345");
-  }
-  @Value  //Дата класс CardId и его методы
-  public static class CardId {
-    private String id;
-  }
+    public static VerificationCode getVerificationCodeFor() {
+        return new VerificationCode("12345");
+    }
 
-  public static CardId getFirstCardId() {
-    return new CardId("92df3f1c-a033-48e6-8390-206f6b1f56c0");
-  }
+    @Value  //Дата класс CardId и его методы
+    public static class CardId {
+        private String id;
+    }
 
-  public static CardId getSecondCardId() {
-    return new CardId("0f3f5c2a-249e-4c3d-8287-09f7a039391d");
-  }
+    public static CardId getFirstCardId() {
+        return new CardId("92df3f1c-a033-48e6-8390-206f6b1f56c0");
+    }
 
-  @Value
-  public static class TransferInfo {
-    private int amount;
-    private String numberCard;
-  }
+    public static CardId getSecondCardId() {
+        return new CardId("0f3f5c2a-249e-4c3d-8287-09f7a039391d");
+    }
 
-  public static TransferInfo getFirstCardTransferInfoPositive() {
-    return new TransferInfo(7_401, "5559 0000 0000 0002");
-  }
+    @Value
+    public static class TransferInfo {
+        private int amount;
+        private String numberCard;
+    }
 
-  public static TransferInfo getSecondCardTransferInfoPositive() {
-    return new TransferInfo(4_068, "5559 0000 0000 0001");
-  }
+    public static TransferInfo getFirstCardTransferInfoPositive() {
+        return new TransferInfo(6_501, "5559 0000 0000 0002");
+    }
 
-  public static TransferInfo getFirstCardTransferInfoNegative() {
-    return new TransferInfo(-906, "5559 0000 0000 0002");
-  }
+    public static TransferInfo getSecondCardTransferInfoPositive() {
+        return new TransferInfo(4_669, "5559 0000 0000 0001");
+    }
 
-  public static TransferInfo getSecondCardTransferInfoNegative() {
-    return new TransferInfo(21_739, "5559 0000 0000 0001");
-  }
+    public static TransferInfo getFirstCardTransferInfoNegative() {
+        return new TransferInfo(-906, "5559 0000 0000 0002");
+    }
 
-  public static TransferInfo setFirstCardTransferInfo(int amountTransfer) {
-    return new TransferInfo(amountTransfer, "5559 0000 0000 0002");
-  }
+    public static TransferInfo getSecondCardTransferInfoNegative() {
+        return new TransferInfo(21_000, "5559 0000 0000 0001");
+    }
 
-  public static TransferInfo setSecondCardTransferInfo(int amountTransfer) {
-    return new TransferInfo(amountTransfer, "5559 0000 0000 0001");
-  }
+    public static TransferInfo setFirstCardTransferInfo(int amountTransfer) {
+        return new TransferInfo(amountTransfer, "5559 0000 0000 0002");
+    }
+
+    public static TransferInfo setSecondCardTransferInfo(int amountTransfer) {
+        return new TransferInfo(amountTransfer, "5559 0000 0000 0001");
+    }
 }
-}
+
